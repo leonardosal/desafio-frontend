0db1e3f59e9d55e5079b2eab06991eee98080d07
Leonardo Almeida <leonardo.almeida18@gmail.com>
2018-08-25 00:40:30 -0300

    Update constants.js

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,7 +1,7 @@
 (function(){
 	'use strict';
 	angular.module('app').constant('Constants', {
-		BASE_URL: 'https://api.dribbble.com/v2/shots',
+		BASE_URL: 'https://api.dribbble.com/v2/popular_shots',
 		TOKEN: 'aa5bc5eb00605db64ba35293661b34b946cd94cdff7deb40375d87a9bf98f2d0'
 	});
 })();
